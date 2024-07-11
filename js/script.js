98d721ce4a2c0dd2f4830c31ecf194e93cc7ae21
ben <ben@planetgrafix.com>
2024-07-10 22:50:52 -0700

    refactor

--- js/script.js
+++ js/script.js
@@ -1,73 +1,6 @@
 (function ($) {
   $(document).ready(function () {
-    console.log("Loaded 1");
-
-    function split_type() {
-      // Split text into spans
-      let typeSplit = new SplitType("[text-split]", {
-        types: "words, chars",
-        tagName: "span",
-      });
-
-      // Link timelines to scroll position
-      function createScrollTrigger(triggerElement, timeline) {
-        // Reset tl when scroll out of view past bottom of screen
-        ScrollTrigger.create({
-          trigger: triggerElement,
-          start: "top bottom",
-          onLeaveBack: () => {
-            timeline.progress(0);
-            timeline.pause();
-          },
-        });
-        // Play tl when scrolled into view (60% from top of screen)
-        ScrollTrigger.create({
-          trigger: triggerElement,
-          start: "top 60%",
-          onEnter: () => timeline.play(),
-        });
-      }
-
-      $("[words-slide-up]").each(function (index) {
-        let tl = gsap.timeline({ paused: true });
-        tl.from($(this).find(".word"), {
-          opacity: 0,
-          yPercent: 100,
-          duration: 1.2,
-          ease: "expo.out",
-          stagger: { amount: 0.6 },
-        });
-        createScrollTrigger($(this), tl);
-      });
-
-      $("[letters-slide-up]").each(function (index) {
-        let tl = gsap.timeline({ paused: true });
-        tl.from($(this).find(".char"), {
-          opacity: 0,
-          yPercent: 100,
-          duration: 1.8,
-          ease: "expo.out",
-          stagger: { amount: 0.6 },
-        });
-        createScrollTrigger($(this), tl);
-      });
-
-      $("[letters-fade-in]").each(function (index) {
-        let tl = gsap.timeline({ paused: true });
-        tl.from($(this).find(".char"), {
-          opacity: 0,
-          duration: 0.2,
-          ease: "power1.out",
-          stagger: { amount: 0.8 },
-        });
-        createScrollTrigger($(this), tl);
-      });
-
-      // Avoid flash of unstyled content
-      gsap.set("[text-split]", { opacity: 1 });
-    }
-
-    split_type();
+    console.log("Loaded");
 
     function intro_animation() {
       function attachAnim(container, path) {
@@ -135,6 +68,73 @@
 
     intro_animation();
   });
+
+  function split_type() {
+    // Split text into spans
+    let typeSplit = new SplitType("[text-split]", {
+      types: "words, chars",
+      tagName: "span",
+    });
+
+    // Link timelines to scroll position
+    function createScrollTrigger(triggerElement, timeline) {
+      // Reset tl when scroll out of view past bottom of screen
+      ScrollTrigger.create({
+        trigger: triggerElement,
+        start: "top bottom",
+        onLeaveBack: () => {
+          timeline.progress(0);
+          timeline.pause();
+        },
+      });
+      // Play tl when scrolled into view (60% from top of screen)
+      ScrollTrigger.create({
+        trigger: triggerElement,
+        start: "top 60%",
+        onEnter: () => timeline.play(),
+      });
+    }
+
+    $("[words-slide-up]").each(function (index) {
+      let tl = gsap.timeline({ paused: true });
+      tl.from($(this).find(".word"), {
+        opacity: 0,
+        yPercent: 100,
+        duration: 1.2,
+        ease: "expo.out",
+        stagger: { amount: 0.6 },
+      });
+      createScrollTrigger($(this), tl);
+    });
+
+    $("[letters-slide-up]").each(function (index) {
+      let tl = gsap.timeline({ paused: true });
+      tl.from($(this).find(".char"), {
+        opacity: 0,
+        yPercent: 100,
+        duration: 1.8,
+        ease: "expo.out",
+        stagger: { amount: 0.6 },
+      });
+      createScrollTrigger($(this), tl);
+    });
+
+    $("[letters-fade-in]").each(function (index) {
+      let tl = gsap.timeline({ paused: true });
+      tl.from($(this).find(".char"), {
+        opacity: 0,
+        duration: 0.2,
+        ease: "power1.out",
+        stagger: { amount: 0.8 },
+      });
+      createScrollTrigger($(this), tl);
+    });
+
+    // Avoid flash of unstyled content
+    gsap.set("[text-split]", { opacity: 1 });
+  }
+
+  split_type();
 })(jQuery);
 
 /* =========== LENIS SMOOTH SCROL ========== */
